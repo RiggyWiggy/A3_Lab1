15c42135a5f68d1d5fbbe6d334731b54907fd4f5
Brown <a_brown24@tss-hr420-dm19.fc.ca>
2017-03-08 12:32:42 -0500

    update week 9

--- js/master.js
+++ js/master.js
@@ -1,13 +1,43 @@
 (function(){
 	$('.thumbInfo img').on('click', function () {
 
-//add JSON call here
+		//add JSON call here
+		$.ajax({
+			url: "includes/ajaxQuery.php",
+			data: { model: this.id },
+			type: "GET"
+		}) // don't put a semicolon here so we can chain methods together
 
-$.getJSON('includes/ajaxQuery.php', {model:this.id}, function(data) {console.log(data);
-	$('.modelName').text(data.modelName);
-	$('.priceInfo').text(data.pricing);
-	$('.modelDetails').text(data.modelDetails);
+		.done(function(data){
+			console.log(data);
 
+			if(data) {
+
+			} else {
+				alert('your ajax call didn\'t work');
+			}
+
+
+			data= JSON.parse(data);
+			renderCarInfo(data);
+		})
+
+		.fail(function(ajaxCall, status, error){
+			console.log(status, ", ", error);
+			console.dir(ajaxCall);
+		}); //terminate the ajax function
+
+		function renderCarInfo(car) {
+			$('.thumbInfo img').addClass('nonActive');
+			$('#' + car.model).removeClass('nonActive');
+
+
+
+
+			$('.subhead span').text(" mini Cooper " + car.model);
+			$('.modelName').text(car.modelName);
+			$('.priceInfo').text(car.pricing);
+			$('.modelDetails').text(car.modelDetails);
+		}
 	});
-});
 })();
